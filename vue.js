bee62359b41294ef9aa598909bd39b1bc569f96d
Dawid <Dawid@MacBook-Pro-Dawid.local>
2018-09-08 10:19:16 +0200

    [#1.1] Create Vue.js app with list stored in Vue data

--- vue.js
+++ vue.js
@@ -1,3 +1,24 @@
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        'headline': 'Vue.js DevMeetings',
+        'products': [
+            {
+                id: 1,
+                value: 'task1'
+            },
+            {
+                id: 2,
+                value: 'task2'
+            },
+            {
+                id: 3,
+                value: 'task3'
+            },
+            {
+                id: 4,
+                value: 'task4'
+            },
+        ]
+    }
 })
